ebc6436d408088c644410ed8707771b2a7aeb6e7
pcaralc <pcralcaraz@gmail.com>
2022-11-09 10:36:06 +0100

    menu

--- tem4/controlador.php
+++ tem4/controlador.php
@@ -125,7 +125,7 @@ if ($_GET) {
         try {
             $valor = 'tfsygxjvtwmkmmfb';
             //Server settings
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->SMTPDebug = SMTP::DEBUG_OFF;                      //Enable verbose debug output
             $mail->isSMTP();                                            //Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
